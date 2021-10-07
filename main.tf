389092d2052af542b72ee61c8164108924810422
rajeevkghosh <54052253+rajeevkghosh@users.noreply.github.com>
2021-10-07 15:24:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  access_token = var.access_token
+  project      = "tokyo-nomad-323506"
+}
+
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "secret"
+
+  labels = {
+    label = "my-label"
+  }
+
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+      replicas {
+        location = "us-east1"
+      }
+    }
+  }
+}
